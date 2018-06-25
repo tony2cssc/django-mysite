8cfa0cd6952c39eed125fd9f8631cbdbc4f4c59c
tony2cssc <jijing.guo@gmail.com>
2018-06-26 00:08:05 +0800

    finish 1st part of tutorial 4

--- polls/models.py
+++ polls/models.py
@@ -3,11 +3,12 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    
+
     def __str__(self):
         return self.question_text
 
